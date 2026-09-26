b21a1d50e2076e00b41bd4a9be476aa5996b9619
esther <mmesomaesther42@gmail.com>
2026-09-26 16:42:58 +0100

    update project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Landingpages from "./pages/landingpages";
 import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Landingpages />} />
       </Routes>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const HEADER = () => {
+  return (
+    <div>
+      <section>
+        <header>
+          <div className="nav-link-text">
+            <Link to="/">HOME</Link>
+          </div>
+          <div className="nav-link-text">
+            <Link to="/About-Us">ABOUT</Link>
+          </div>
+          <div className="nav-link-text">
+            <Link to="/contact-us">CONTACT</Link>
+          </div>
+          <div className="nav-link-text">
+            <Link to="/Services">SERVICES</Link>
+          </div>
+        </header>
+      </section>
+    </div>
+  );
+};
+
+export default HEADER;
